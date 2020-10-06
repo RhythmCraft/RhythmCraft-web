bb76d355fba1f86df2e0e3902391e3ced68ee136
Hyunsoo, Chung <admin@hyonsu.com>
2020-10-06 12:55:17 +0900

    Add copy comment url

---
+++ public/js/workshop_note.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    $("[data-toggle=popover]").popover();
+}
+
+function copyToClipboard(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
