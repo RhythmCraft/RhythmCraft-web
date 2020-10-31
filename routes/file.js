63eb838b7a54751d1a6e5694887c33e41f319c34
Hyunsoo, Chung <admin@hyonsu.com>
2020-10-31 18:41:52 +0900

    Keep search when remove note, keep search when update music

--- routes/file.js
+++ routes/file.js
@@ -39,11 +39,11 @@ app.post('/uploadmusic', utils.isLogin, upload.single('file'), async (req, res, 
     const filetype = await fileType.fromBuffer(req.file.buffer);
     if(!filetype || !filetype.mime.startsWith('audio/')) {
         req.flash('Error', '음악 파일이 아닙니다.');
-        return res.redirect('/music');
+        return res.redirect(req.get('referrer'));
     }
     if(req.file.size > 1024 * 1024 * 100) {
         req.flash('Error', '파일이 100MB를 초과합니다.');
-        return res.redirect('/music');
+        return res.redirect(req.get('referrer'));
     }
 
     const name = `${uniqueString()}${path.extname(req.file.originalname)}`;
@@ -57,33 +57,33 @@ app.post('/uploadmusic', utils.isLogin, upload.single('file'), async (req, res, 
     });
 
     req.flash('Info', `파일 ${req.file.originalname}이(가) 성공적으로 업로드되었습니다.`);
-    return res.redirect('/music');
+    return res.redirect(req.get('referrer'));
 });
 
 app.get('/removemusic', utils.isLogin, async (req, res, next) => {
     const file = await File.findOne({ owner : req.user.fullID , name : req.query.name , file_type : 'music' });
     if(!file) {
         req.flash('Error', '해당 파일이 존재하지 않습니다.');
-        return res.redirect('/music');
+        return res.redirect(req.get('referrer'));
     }
     fs.unlinkSync(path.join(setting.SAVE_FILE_PATH, file.name));
     await File.deleteOne({ owner : req.user.fullID , name : req.query.name , file_type : 'music' });
 
     req.flash('Info', `${file.originalname}을(를) 삭제했습니다.`);
     req.app.get('socket_game').to(`user_${req.user.fullID}`).emit('msg', { 'action' : 'updatemusic' });
-    return res.redirect('/music');
+    return res.redirect(req.get('referrer'));
 });
 
 app.get('/listenmusic', async (req, res, next) => {
     const file = await File.findOne({ name : req.query.name , file_type : 'music' });
     if(!file) {
         req.flash('Error', '해당 파일이 존재하지 않습니다.');
-        if(req.isAuthenticated()) return res.redirect('/music');
+        if(req.isAuthenticated()) return res.redirect(req.get('referrer'));
         else return res.redirect('/');
     }
     if((!req.isAuthenticated() || file.owner != req.user.fullID) && !file.public) {
         req.flash('Error', '권한이 없습니다.');
-        return res.redirect('/music');
+        return res.redirect(req.get('referrer'));
     }
     return res.sendFile(path.join(setting.SAVE_FILE_PATH, req.query.name));
 });
@@ -92,12 +92,12 @@ app.get('/listenmusic/:music', async (req, res, next) => {
     const file = await File.findOne({ name : req.params.music , file_type : 'music' });
     if(!file) {
         req.flash('Error', '해당 파일이 존재하지 않습니다.');
-        if(req.isAuthenticated()) return res.redirect('/music');
+        if(req.isAuthenticated()) return res.redirect(req.get('referrer'));
         else return res.redirect('/');
     }
     if((!req.isAuthenticated() || file.owner != req.user.fullID) && !file.public) {
         req.flash('Error', '권한이 없습니다.');
-        return res.redirect('/music');
+        return res.redirect(req.get('referrer'));
     }
     return res.sendFile(path.join(setting.SAVE_FILE_PATH, req.params.music));
 });
@@ -106,13 +106,13 @@ app.get('/musicstatus', utils.isLogin, async (req, res, next) => {
     const file = await File.findOne({ name : req.query.name , owner : req.user.fullID , file_type : 'music' });
     if(!file) {
         req.flash('Error', '해당 파일이 존재하지 않습니다.');
-        return res.redirect('/music');
+        return res.redirect(req.get('referrer'));
     }
 
     await File.updateOne({ name : req.query.name , owner : req.user.fullID , file_type : 'music' } , { public : req.query.public == 'true' });
     req.flash('Info', `${file.originalname} 파일 공개 상태가 업데이트되었습니다.`);
     req.app.get('socket_game').to(`user_${req.user.fullID}`).emit('msg', { 'action' : 'updatemusic' });
-    return res.redirect('/music');
+    return res.redirect(req.get('referrer'));
 });
 
 app.get('/note', utils.isLogin, async (req, res, next) => {
@@ -131,11 +131,11 @@ app.get('/note', utils.isLogin, async (req, res, next) => {
 app.post('/uploadnote', utils.isLogin, upload.single('file'), async (req, res, next) => {
     if(req.file.mimetype != 'application/octet-stream' || (path.extname(req.file.originalname) != '.rhythmcraft' && path.extname(req.file.originalname) != '.signedrhythmcraft')) {
         req.flash('Error', '채보 파일이 아닙니다.');
-        return res.redirect('/note');
+        return res.redirect(req.get('referrer'));
     }
     if(req.file.size > 1024 * 1024 * 100) {
         req.flash('Error', '파일이 100MB를 초과합니다.');
-        return res.redirect('/note');
+        return res.redirect(req.get('referrer'));
     }
 
     const name = `${uniqueString()}${path.extname(req.file.originalname)}`;
@@ -151,14 +151,14 @@ app.post('/uploadnote', utils.isLogin, upload.single('file'), async (req, res, n
 
     req.flash('Info', `채보 ${req.file.originalname}이(가) 성공적으로 업로드되었습니다.`);
     req.app.get('socket_game').to(`user_${req.user.fullID}`).emit('msg', { 'action' : 'updatenote' });
-    return res.redirect('/note');
+    return res.redirect(req.get('referrer'));
 });
 
 app.get('/downloadnote', utils.isLogin, async (req, res, next) => {
     const file = await File.findOne({ owner : req.user.fullID , name : req.query.name , file_type : 'note' });
     if(!file) {
         req.flash('Error', '해당 채보가 존재하지 않습니다.');
-        return res.redirect('/note');
+        return res.redirect(req.get('referrer'));
     }
     return res.download(path.join(setting.SAVE_FILE_PATH, file.name), file.originalname);
 });
@@ -167,7 +167,7 @@ app.get('/removenote', utils.isLogin, async (req, res, next) => {
     const file = await File.findOne({ owner : req.user.fullID , name : req.query.name , file_type : 'note' });
     if(!file) {
         req.flash('Error', '해당 채보가 존재하지 않습니다.');
-        return res.redirect('/note');
+        return res.redirect(req.get('referrer'));
     }
     fs.unlinkSync(path.join(setting.SAVE_FILE_PATH, file.name));
     await File.deleteOne({ owner : req.user.fullID , name : req.query.name , file_type : 'note' });
@@ -181,7 +181,7 @@ app.get('/removenote', utils.isLogin, async (req, res, next) => {
 
     req.flash('Info', `${file.originalname}을(를) 삭제했습니다.`);
     req.app.get('socket_game').to(`user_${req.user.fullID}`).emit('msg', { 'action' : 'updatenote' });
-    return res.redirect('/note');
+    return res.redirect(req.get('referrer'));
 });
 
 app.get('/select_music', utils.isLogin, async (req, res, next) => {
@@ -202,7 +202,7 @@ app.post('/savenote', utils.isLogin, async (req, res, next) => {
     const file = await File.findOne({ owner : req.user.fullID , name : req.body.name , file_type : 'note' });
     if(!file) {
         req.flash('Error', '해당 채보가 존재하지 않습니다.');
-        return res.redirect('/note');
+        return res.redirect(req.get('referrer'));
     }
 
     fs.writeFileSync(path.join(setting.SAVE_FILE_PATH, file.name), JSON.stringify(req.body.note));
@@ -213,7 +213,7 @@ app.post('/savenote', utils.isLogin, async (req, res, next) => {
 app.post('/signnote', utils.isAdmin, upload.single('file'), async (req, res, next) => {
     if(req.file.mimetype != 'application/octet-stream' || path.extname(req.file.originalname) != '.rhythmcraft') {
         req.flash('Error', '채보 파일이 아닙니다.');
-        return res.redirect('/note');
+        return res.redirect(req.get('referrer'));
     }
     if(req.file.size > 1024 * 1024 * 100) {
         req.flash('Error', '파일이 100MB를 초과합니다.');
