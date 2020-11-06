2207a55381960e100d690a0533015a09a5a03d28
Hyunsoo, Chung <admin@hyonsu.com>
2020-11-06 11:46:48 +0900

    Fix kick always send chat self kick

--- socket/game.js
+++ socket/game.js
@@ -425,16 +425,16 @@ module.exports = (io, app) => {
 
         socket.on('kickUser', async data => {
             if(master) {
-                const user = await RoomUser.findOne({ fullID : data.fullID });
                 if(data.fullID == user.fullID) return socket.emit('msg', {
                     'action': 'alert',
                     'message': '자신을 킥할 수 없습니다.'
                 });
+                const kickuser = await RoomUser.findOne({ fullID : data.fullID });
                 io.to(`user_${data.fullID}`).emit('msg', {
                     'action': 'exit',
                     'message': '방장에게 강퇴되었습니다.'
                 });
-                io.connected[user.socket_id].disconnect();
+                io.connected[kickuser.socket_id].disconnect();
                 await RoomUser.deleteOne({ fullID : data.fullID });
             }
         });
