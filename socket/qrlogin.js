3ab43c1addcc02d28e911dd12ac7895ec57bf811
Hyunsoo, Chung <admin@hyonsu.com>
2021-05-22 00:21:15 +0900

    Fix qrlogin

--- socket/qrlogin.js
+++ socket/qrlogin.js
@@ -2,6 +2,6 @@ const setting = require('../setting.json');
 
 module.exports = io => {
     io.on('connection', socket => {
-        socket.emit('msg', { "action" : "saveid" , "id" : socket.id.replace('#', '<sh>') , "port" : setting.PORT });
+        socket.emit('msg', { "action" : "saveid" , "id" : socket.id.replace('#', '<sh>') , "port" : setting.ENFORCE_SOCKET_STANDARD_PORT ? (setting.USE_SSL ? 443 : 80) : setting.PORT });
     });
 }
